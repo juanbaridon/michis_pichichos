db44e0b579af74b6759eda75686c8e969b557a18
juanbaridon <juanbaridonjd@hotmail.com>
2023-09-10 19:02:35 -0300

    correcciones

--- script.js
+++ script.js
@@ -2,34 +2,26 @@ const btnGatos = document.getElementById('btnGatos');
 const btnPerros = document.getElementById('btnPerros');
 const contenedorImagen = document.getElementById('imagen');
 
-btnGatos.addEventListener('click', () => {
-    fetch('https://api.thecatapi.com/v1/images/search')
+
+function fetchAdorable (url) {
+    fetch(url)
         .then(response => response.json())
         .then(data => {
-            const imagenGato = document.createElement('img');
-            imagenGato.src = data[0].url;
+            const imagenAdorable = document.createElement('img');
+            imagenAdorable.src = data[0].url;
             contenedorImagen.innerHTML = '';
-            contenedorImagen.appendChild(imagenGato);
+            contenedorImagen.appendChild(imagenAdorable);
         })
         .catch(error => {
-            console.error('Error al obtener la imagen de gato:', error);
+            console.error('Error al obtener la imagen:', error);
             contenedorImagen.innerHTML = '';
             contenedorImagen.innerHTML = '<p>No se pudo cargar la imágen correctamente.</p>';
         });
-});
+}
+btnGatos.addEventListener('click', () => {
+    fetchAdorable('https://api.thecatapi.com/v1/images/search')
+})
 
 btnPerros.addEventListener('click', () => {
-    fetch('https://api.thedogapi.com/v1/images/search')
-        .then(response => response.json())
-        .then(data => {
-            const imagenPerro = document.createElement('img');
-            imagenPerro.src = data[0].url;
-            contenedorImagen.innerHTML = '';
-            contenedorImagen.appendChild(imagenPerro);
-        })
-        .catch(error => {
-            console.error('Error al obtener la imagen de perro:', error);
-            contenedorImagen.innerHTML = '';
-            contenedorImagen.innerHTML = '<p>No se pudo cargar la imágen correctamente.</p>';
-        });
+    fetchAdorable('https://api.thedogapi.com/v1/images/search')
 });
